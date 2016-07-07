f8e174b155ee6b0c2da80a1119281813552e4545
bhok <bhokor@gmail.com>
2016-07-07 04:44:24 +0200

    Update bitcoin.cpp

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -80,7 +80,7 @@ class CNode {
     CAddress me(CService("0.0.0.0"));
     BeginMessage("version");
     int nBestHeight = GetRequireHeight();
-    string ver = "/CryptoJacks-Seeds:1.0/";
+    string ver = "/cjseeds:1.0/";
     vSend << PROTOCOL_VERSION << nLocalServices << nTime << you << me << nLocalNonce << ver << nBestHeight;
     EndMessage();
   }
