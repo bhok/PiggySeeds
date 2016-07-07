5e77993f9c3854c66007029b6732a1fefe6bde06
bhok <bhokor@gmail.com>
2016-07-07 04:54:51 +0200

    Update protocol.cpp

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xa1, 0xa0, 0xa2, 0xa3 };
+unsigned char pchMessageStart[4] = { 0x5a, 0xc3, 0x82, 0xd3 };
 
 CMessageHeader::CMessageHeader()
 {
